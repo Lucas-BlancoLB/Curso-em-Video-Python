26e3aea25c0b83d80883d0837b4008cc238bb146
Lucas-BlancoLB <113478537+Lucas-BlancoLB@users.noreply.github.com>
2022-09-16 18:42:39 -0300

    Add files via upload
    Conceitos que elaborei na aula

---
+++ aula06a.py
@@ -0,0 +1,11 @@
+# Desafio extra faça uma soma q a msg diga soma entre n1 e n2 da x
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1 + n2
+print(f'A soma entre {n1} mais {n2} é {soma}!')
+
+
+# Uso de type() para saber o tipo primitivo da variável
+
+# n1 = int(input('Insira um valor: '))
+# print(type(n1))
